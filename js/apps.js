4fe13d630605becf9291f05f4fe0070430b3755e
MST FARDOUES <MST FARDOUES>
2016-01-13 09:28:13 -0600

    add Enter key

--- js/apps.js
+++ js/apps.js
@@ -1,37 +1,46 @@
 $(document).ready(function(){
+
 	$('#Add').click(function(){
 		var text =$('#textarea').val();
-		$('.likha').append('<li><a><input type="checkbox" class="cross"/>'+text+'<button class="del">Remove</button></a></li>');
-		
+		$('.likha').append('<li><a> <input type="checkbox" class="cross"/>'+text+'<button class="del">Remove</button></a></li>');
 		$('#textarea').val('');
+
 	});
+
+
+	$(document).keydown(function(e){
+		if(e.keyCode ==13){
+		var text =$('#textarea').val();
+		$('.likha').append('<li><a> <input type="checkbox" class="cross"/>'+text+'<button class="del">Remove</button></a></li>');
+		$('#textarea').val('');
+}
+	});
+
 
 
 
 
 	$('#ren').click(function(){
-
 		$('li').remove();
 
 	});
 
-	
+
+
 
 	$(document).on('click', '.del', function(){
-
 		$(this).parent().remove();
 
 	});
 
 
 	$(document).on('click', '.cross', function(){
-		if( $(this).parent().css('textDecoration') =='line-through' ){
+		if( $(this).parent().css('textDecoration') =='line-through'){
 			$(this).parent().css('textDecoration','none');
 
 
 		}
 		else{
-
 			$(this).parent().css('textDecoration','line-through');
 
 		}
