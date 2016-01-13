34180cf467d64004931706fa6ce4aa707e350461
MST FARDOUES <MST FARDOUES>
2016-01-12 23:19:28 -0600

    jQuery included

--- js/apps.js
+++ js/apps.js
@@ -1,12 +1,45 @@
-$(document).ready(function(
-	$('.checkout').animate({
-		'width': 1400,
-		'height':1200
+$(document).ready(function(){
+	$('#Add').click(function(){
+		var text =$('#textarea').val();
+		$('.likha').append('<li><a><input type="checkbox" class="cross"/>'+text+'<button class="del">Remove</button></a></li>');
+		
+		$('#textarea').val('');
+	});
 
 
 
-	},5000);
+
+	$('#ren').click(function(){
+
+		$('li').remove();
+
+	});
+
+	
+
+	$(document).on('click', '.del', function(){
+
+		$(this).parent().remove();
+
+	});
+
+
+	$(document).on('click', '.cross', function(){
+		if( $(this).parent().css('textDecoration') =='line-through' ){
+			$(this).parent().css('textDecoration','none');
+
+
+		}
+		else{
+
+			$(this).parent().css('textDecoration','line-through');
+
+		}
 
 
 
-	));
+	});
+
+
+
+});
